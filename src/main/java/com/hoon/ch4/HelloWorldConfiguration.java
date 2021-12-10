bd0f0b854defc2baca360da8bdcee20ed017a33b
변구훈 <byeonguhun@byeonguhun-ui-MacBookPro.local>
2021-12-11 00:26:26 +0900

    Initial commit

---
+++ src/main/java/com/hoon/ch4/HelloWorldConfiguration.java
@@ -0,0 +1,38 @@
+package com.hoon.ch4;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class HelloWorldConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("helloJob")
+                .start(step1())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("Hello, world!");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+}
